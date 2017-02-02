1a580c678faa5cb52a6f1d77d2c8a7d653e6acd7
choury <zhouwei400@gmail.com>
2017-02-02 17:53:00 +0800

    每次请求重置curl_handle

--- net.c
+++ net.c
@@ -167,27 +167,27 @@ static CURL* getcurl(){
         conhead=conhead->next;
         curl=tmp->handle;
         free(tmp);
-        pthread_mutex_unlock(&lockcon);
+        curl_easy_reset(curl);
     }else{                                  //否则新生成一个，设定参数
-        pthread_mutex_unlock(&lockcon);
         curl=curl_easy_init();
-        curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, my_trace);
-        curl_easy_setopt(curl, CURLOPT_DEBUGDATA, &config);
-        curl_easy_setopt(curl, CURLOPT_FILETIME, 1);
-        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
-        curl_easy_setopt(curl, CURLOPT_FRESH_CONNECT, 0);
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-        curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 1);
-        curl_easy_setopt(curl, CURLOPT_CLOSEPOLICY, CURLCLOSEPOLICY_LEAST_RECENTLY_USED);
-        curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 5);
-        curl_easy_setopt(curl, CURLOPT_HEADER, 0);
-        curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_WHATEVER);
-        curl_easy_setopt(curl, CURLOPT_LOW_SPEED_LIMIT, 5);
-        curl_easy_setopt(curl, CURLOPT_LOW_SPEED_TIME, 10);
-        curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 120);
-        curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
     }
+    pthread_mutex_unlock(&lockcon);
+    curl_easy_setopt(curl, CURLOPT_DEBUGFUNCTION, my_trace);
+    curl_easy_setopt(curl, CURLOPT_DEBUGDATA, &config);
+    curl_easy_setopt(curl, CURLOPT_FILETIME, 1);
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1);
+    curl_easy_setopt(curl, CURLOPT_FRESH_CONNECT, 0);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 1);
+    curl_easy_setopt(curl, CURLOPT_CLOSEPOLICY, CURLCLOSEPOLICY_LEAST_RECENTLY_USED);
+    curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 5);
+    curl_easy_setopt(curl, CURLOPT_HEADER, 0);
+    curl_easy_setopt(curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_WHATEVER);
+    curl_easy_setopt(curl, CURLOPT_LOW_SPEED_LIMIT, 5);
+    curl_easy_setopt(curl, CURLOPT_LOW_SPEED_TIME, 10);
+    curl_easy_setopt(curl, CURLOPT_CONNECTTIMEOUT, 120);
+    curl_easy_setopt(curl, CURLOPT_NOSIGNAL, 1);
+    curl_easy_setopt(curl, CURLOPT_VERBOSE, 0);
     return curl;
 }
 
